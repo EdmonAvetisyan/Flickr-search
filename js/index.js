796105f72258bd3e748dfa129aa329a99144dbdd
Edmon Avetisyan <edmonavetisyan9@gmail.com>
2019-08-08 21:02:32 +0400

    implementation ajax request–response and form validation

--- js/index.js
+++ js/index.js
@@ -1,1 +1,88 @@
-document.write( 'world' );
+$(function () {
+
+	$('#searchImageForm').on('submit', getImages);
+
+	function getImages(e) {
+		e.preventDefault();
+		const tag = $(e.target.searchinput)
+			.val()
+			.trim()
+			.toLowerCase();
+
+		if(!tag.length){
+			$('#result-contain').html('');
+			$('#searchImageForm .invalid-feedback')
+				.text('Please enter image name . . .')
+				.show();
+			return;	
+		}
+		
+		renderResult();
+		jqxhr(tag);
+	}
+
+	function renderResult(whatToRender) {
+
+		const spinner = $('.spinner-border');
+		const resultContain = $('#result-contain');
+		const invalidFeedback = $('#searchImageForm .invalid-feedback');
+
+		invalidFeedback.hide();
+		resultContain.html('');
+		spinner.hide();
+
+		if (!window.navigator.onLine) {
+			invalidFeedback.text('No internet connection . . .').show();
+			resultContain.html('');
+			return;
+		}
+
+		invalidFeedback.hide();
+		resultContain.html('');
+		spinner.show();
+
+		if (whatToRender && whatToRender.items.length === 0) {
+			invalidFeedback.text('Image not found . . . ').show();
+			resultContain.html('');
+			spinner.hide();
+			return;
+		}
+
+		if (whatToRender && whatToRender != 'error') {
+			invalidFeedback.hide();
+			resultContain.html('');
+			spinner.hide();
+
+			$.each(whatToRender.items, function (i, item) {
+				$("<img />").attr("src", item.media.m).appendTo('#result-contain');
+				if (i === 3) {
+					return false;
+				}
+			});
+		} else if (whatToRender == 'error') {
+			invalidFeedback.text('Error ...  Please try again').show();
+			resultContain.html('');
+			spinner.hide();
+		}
+	}
+
+	function jqxhr(tag) {
+		const API_URL = "https://www.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+
+		$.getJSON (
+			API_URL, 
+			{
+				format: "json",
+				tags: tag
+			}
+		)
+			.done(function (data) {
+				console.log(data);
+				renderResult(data);
+			})
+			.fail(function (jqxhr, textStatus) {
+				renderResult(textStatus);
+			});
+	}
+
+});
